46b53b5d7c251629d96ed0297e9222ae99ab520f
Aman Gupta <amangupta@Aman-ka-Mac-mini.local>
2022-10-12 23:42:22 +0530

    switched to new db

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql2');
 //improve this to cred files
 let connection = mysql.createConnection({
-    host:"localhost",
+    host:"65.2.187.174",
     port:3306,
     database:"squareboat_task",
     user:"root",

df8983158562c795f54c999f56b4a386cac2608d
Aman Gupta <amangupta@Aman-ka-Mac-mini.local>
2022-10-14 07:59:49 +0530

    Optimize

--- frontend/src/services/reloadFreshStates.js
+++ frontend/src/services/reloadFreshStates.js
@@ -7,29 +7,14 @@ const { getFollowers, getFollowing, getNotFollowing, getFeed } = require("./apiS
 
 
 async function reloadFreshStates(dispatch){
-    // getFollowers().then(response=>{
-    //     dispatch(setFollowers(response.followers))
-    // })
-    // getFollowing().then(response=>{
-    //     dispatch(setFollowers(response.usersFollowing))
-    // })
-    // getNotFollowing().then(response=>{
-    //     dispatch(setUsersFollowing(response.usersNotFollowing))
-    // })
-    // getFeed().then(response=>{
-    //     dispatch(setPosts(response.posts))
-    // })
+    
     let [{followers},{usersFollowing},{usersNotFollowing},{posts}] = await Promise.all([
         getFollowers(),
         getFollowing(),
         getNotFollowing(),
         getFeed()
     ])
-    // let followers = (await getFollowers()).followers
-    // let usersFollowing = (await getFollowers()).usersFollowing
-    // let usersNotFollowing = (await getNotFollowing()).usersNotFollowing
-    // let posts = (await getNotFollowing()).posts
-    // console.log(followers)
+    
     dispatch(setFollowers(followers))
     dispatch(setUsersFollowing(usersFollowing))
     dispatch(setUsersNotFollowing(usersNotFollowing))

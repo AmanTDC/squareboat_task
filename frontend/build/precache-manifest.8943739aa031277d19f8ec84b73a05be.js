6c402b326dfbb53f26fb9193a1747fc34ed25ed4
Aman Gupta <amangupta@Aman-ka-Mac-mini.local>
2022-10-18 18:27:25 +0530

    new

---
+++ frontend/build/precache-manifest.8943739aa031277d19f8ec84b73a05be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224a5ce31a64bb7ef121fb92033bb23b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55ebdb265d5749abd4fe",
+    "url": "/static/css/main.809ee768.chunk.css"
+  },
+  {
+    "revision": "f6a959482b87f3aaf008",
+    "url": "/static/js/2.b56efadc.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/static/js/2.b56efadc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ebdb265d5749abd4fe",
+    "url": "/static/js/main.913cc866.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.913cc866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
